1c8dcc46b4baafc075774230600c1ef7b5b8a002
infinity4747 <infinity2462@gmail.com>
2019-09-26 10:19:45 +0600

    first version

---
+++ login/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^login/$', LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^logout/$',  LogoutView.as_view(template_name='logged_out.html'), name='logout'),
+]
